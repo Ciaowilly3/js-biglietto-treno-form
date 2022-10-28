074eaa2ec300c65e52d5d7a04e0dfbca74bb5032
Ciaowilly3 <muscomassi@gmail.com>
2022-10-28 16:37:06 +0200

    codice funzionante

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ mainBtn.addEventListener( "click", function () {
 const fullNameEl = document.getElementById("fullName");
 const distanceEl = document.getElementById("distance");
 const scontoEl = document.getElementById("sconto");
-
 // prendo il valore dall'html
 const fullName = fullNameEl.value;
 const distance = distanceEl.value;
 const sconto = scontoEl.value
+let prezzoFinale = distance * 0.21;
 
 // inizio a sostituire
 const ticketName =document.getElementById("nome");
@@ -23,8 +23,24 @@ if (sconto==0){
     ticketOffer.innerHTML = " Biglietto Standard"
 }else if (sconto==20){
     ticketOffer.innerHTML = " Offerta Bambini"
+    prezzoFinale = prezzoFinale-(prezzoFinale*(sconto/100));
 }else if (sconto==40){
     ticketOffer.innerHTML = " Offerta over 65"
+    prezzoFinale = prezzoFinale-(prezzoFinale*(sconto/100));
 }
+
+
+// creo variabili random per carrozza e Codice CP
+const carrozza =Math.floor((Math.random() * 20) + 1);
+const carrozzaEl = document.getElementById("carrozza");
+carrozzaEl.innerHTML = carrozza;
+
+const codice =Math.floor((Math.random() * 100000) + 1);
+const codiceEl = document.getElementById("codiceCp");
+codiceEl.innerHTML = codice;
+
+// mando in output il prezzo finale
+const prezzoFinaleEl = document.getElementById("costoBiglietto");
+prezzoFinaleEl.innerHTML = `${prezzoFinale}&euro;`
 
 })
